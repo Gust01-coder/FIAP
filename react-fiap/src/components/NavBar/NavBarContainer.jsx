5ed6c5f4ed8b6438c45e2e091e94ee148d5759c7
Vinícius Costa <vinicius.costa@simplepractice.com>
2025-09-03 19:59:43 -0300

    refactor: navbar classname

--- react-fiap/src/components/NavBar/NavBarContainer.jsx
+++ react-fiap/src/components/NavBar/NavBarContainer.jsx
@@ -1,14 +1,14 @@
 const NavBarContainer = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-white shadow-sm">
-      <div class="container">
-        <a class="navbar-brand" href="index.html">
-          <span class="logo-text">
-            Agro<span class="text-success">Tech</span>
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-white shadow-sm">
+      <div className="container">
+        <a className="navbar-brand" href="index.html">
+          <span className="logo-text">
+            Agro<span className="text-success">Tech</span>
           </span>
         </a>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
@@ -16,37 +16,39 @@ const NavBarContainer = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link active" href="index.html">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link active" href="index.html">
                 Home
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="solucoes.html">
+            <li className="nav-item">
+              <a className="nav-link" href="solucoes.html">
                 Soluções
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="sobre.html">
+            <li className="nav-item">
+              <a className="nav-link" href="sobre.html">
                 Sobre Nós
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="casos.html">
+            <li className="nav-item">
+              <a className="nav-link" href="casos.html">
                 Casos de Sucesso
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contato.html">
+            <li className="nav-item">
+              <a className="nav-link" href="contato.html">
                 Fale Conosco
               </a>
             </li>
           </ul>
-          <button class="btn btn-success ms-lg-3">Experimentar Agora</button>
+          <button className="btn btn-success ms-lg-3">
+            Experimentar Agora
+          </button>
         </div>
       </div>
     </nav>
